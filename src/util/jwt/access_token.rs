602e2e82d738b1b9fdea93106cd6b3c1818c7f71
ju_deu <fabi_deu@icloud.com>
2025-03-10 21:04:34 +0100

    refreshing token

--- src/util/jwt/access_token.rs
+++ src/util/jwt/access_token.rs
@@ -44,7 +44,9 @@ impl AccessToken {
     }
 
 
-    pub fn refresh_token() -> () {
-        todo!()
+    pub fn refresh_token(self, jwt_secret: &String) -> jsonwebtoken::errors::Result<Self> {
+        let old_claims = &self.claims;
+        let new_claims = Claims::new(old_claims.sub, old_claims.tokenversion, 20);
+        AccessToken::from_claims(new_claims, jwt_secret)
     }
 }
